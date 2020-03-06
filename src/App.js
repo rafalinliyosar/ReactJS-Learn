553c5b46f1ce0eca908480bd629e3bce5fae01c8
rafalinliyosar <rafalin.liyosar@gmail.com>
2020-03-06 15:19:57 +0530

    client side routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 import CountComponent from './CountComponent';
+import AboutMeComponent from './AboutMeComponent';
 
 function App() {
   return (
     <div className="App">
-      <CountComponent />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li><Link to="/aboutme">About Me</Link></li>
+          <li><Link to="/learn">Learn</Link></li>
+
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <CountComponent initialCount='5' />
+          </Route>
+          <Route path="/aboutme">
+            <AboutMeComponent name="Rafalin Liyosar" />
+          </Route>
+          <Route path="/learn">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> hello.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Rafalin Learn ReactJS by changing this component
         </a>
-      </header>
+            </header>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
